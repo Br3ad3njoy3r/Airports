3331bec305fcbd4a869cf15e07db57e71cc0e8e0
Caleb Sechler <Caleb.Sechler@trojans.dsu.edu>
2025-03-21 21:32:53 -0500

    feat: changed implementaion to a trie and got reading in from an airport file working.

--- airports.h
+++ airports.h
@@ -15,9 +15,9 @@ struct flights {
     string ID;
 };
 
-using flight = flights;
+using flight = struct flights;
 
-struct airport {
+struct airports {
     string code;
     string name;
     string city;
@@ -25,11 +25,73 @@ struct airport {
     vector<flight> destinations;
 };
 
-using port = airport;
+using port = struct airports;
+
+struct trieNode {
+    trieNode* child[26];
+    bool endKey;
+    port* airport;
+    trieNode() : endKey(false), airport(nullptr) { //constructor for the function
+        for (int i=0;i<26;i++){                   //everytime it makes a new trieNode, this code happens
+            child[i]=nullptr;
+        }
+    }
+};
+
+
 
 class flight_graph {
     public:
-        
+        flight_graph();
+        void Add(port* record);
+        void LookUp(string code);
+        void airportCount();
     private:
-        vector<port> registry;
+        int portCount;
+        trieNode* trieTable;
 };
+
+using graph = flight_graph;
+
+graph::flight_graph() {
+    portCount=0;
+    trieTable = new trieNode();
+}
+
+//methods
+void graph::Add(port* record){
+    trieNode* current = trieTable;
+    for (char c: record->code){
+        if (current->child[c-'A']==nullptr){
+            trieNode* newNode = new trieNode();
+            current->child[c-'A']=newNode;
+        }
+        current=current->child[c-'A'];
+    }
+    if (current->endKey==true){
+        current->airport=record;
+    } else {
+        current->airport=record;
+        portCount++;
+        current->endKey=true;
+    }
+}
+
+void graph::LookUp(string code){
+    trieNode* current = trieTable;
+    for (char c : code){
+        if (current->child[c-'A']==nullptr){
+            cout << "Airport: " << code << "not found." << endl;
+        }
+        current=current->child[c-'A'];
+    }
+    if (current->endKey==true){
+        cout << "Airport: " << current->airport->code << " found." << endl;
+    } else {
+        cout << "Airport: " << code << "not found." << endl;
+    }
+}
+
+void graph::airportCount(){
+    cout << "Total airports: " << portCount << endl;
+}
