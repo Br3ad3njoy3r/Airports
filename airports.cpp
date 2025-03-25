359fc53bf6ce6b1aa495cc84fe8ae27b5518264d
OmegaBlaze33 <OmegaBlaze33@gmail.com>
2025-03-25 08:07:07 -0500

    feat: functionality to read flights file and several lookup functions.

--- airports.cpp
+++ airports.cpp
@@ -2,8 +2,14 @@
 
 int main(int argc, char* argv[]){
     ifstream file;
-    if (argc < 2){
+    if (argc < 2)
+    {
         cout << "No file provided." << endl;
+        return 0;
+    }
+    if (argc < 3)
+    {
+        cout << "Only one file provided." << endl;
         return 0;
     }
     file.open(argv[1]);
@@ -30,5 +36,50 @@ int main(int argc, char* argv[]){
         airportData.Add(tmp);
     }
     file.close();
+
+    file.open(argv[2]);
+    while(getline(file, data, ' '))
+    {
+        flight* tmp = new flight();
+        tmp->source = data;
+
+        getline(file, data, ' ');
+        tmp->destination = data;
+
+        getline(file, data, ' ');
+        tmp->departure = stoi(data);
+
+        getline(file, data, ' ');
+        tmp->arrival = stoi(data);
+
+        getline(file, data, ' ');
+        tmp->cost = stoi(data);
+
+        getline(file, data, ' ');
+        tmp->miles = stoi(data);
+
+        getline(file, data, ' ');
+        tmp->airline = data;
+
+        getline(file, data);
+        tmp->ID = data;
+
+        port* tempPort = new port();
+        tempPort = airportData.returnAirport(tmp->source);
+        tempPort->departures.push_back(*tmp);
+
+        tempPort = airportData.returnAirport(tmp->destination);
+        tempPort->arrivals.push_back(*tmp);
+    
+    }
+    file.close();
+
     airportData.LookUp("ABQ");
+    airportData.LookUp("BOS");
+
+    airportData.listFlightsDeparting("ABQ");
+    airportData.listFlightsArriving("AUS");
+    airportData.listFlightsToAndFromSameAirport("ABQ", "AUS");
+
+    airportData.listAirportsInState("SD");
 }
